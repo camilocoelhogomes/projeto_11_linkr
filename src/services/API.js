20fa957269142e4a375659877e3fb2e7446c3505
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-09-16 17:33:33 -0300

    Created functions to get posts from server

--- src/services/API.js
+++ src/services/API.js
@@ -21,4 +21,20 @@ function signUp({ requestBody, history, setIsLoading }) {
     });
     request.then(res => history.push("/"));
 }
-export { signUp };
+
+const createHeaders = (token) => {
+    return {headers: { Authorization: `Bearer ${token}`}};
+}
+
+const getUserPosts = ({ token, id }) => axios.get(`${API_URL}/users/${id}/posts`, createHeaders(token));
+
+const getLikedPosts = ({ token }) => axios.get(`${API_URL}/posts/liked`, createHeaders(token));
+
+const getHashtagPosts = ({ token, hashtag }) => axios.get(`${API_URL}/hashtags/${hashtag}/posts`, createHeaders(token));
+
+export { 
+    signUp,
+    getUserPosts,
+    getLikedPosts,
+    getHashtagPosts
+};
