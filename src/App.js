1f049417ca64b25458be209bf5fac7e736bfcb4c
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-09-16 17:41:05 -0300

    Created routes and pages empty main components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignUp from "./pages/home/sign-up/index";
 import React from "react";
 import TimeLine from './pages/timeLine';
+import MyPosts from './pages/my-posts';
+import UserPosts from './pages/user-posts';
+import HashtagPosts from './pages/hashtag-posts';
+import LikedPosts from './pages/liked-posts';
 
 function App() {
   return (
@@ -12,9 +16,20 @@ function App() {
         <Route exact path="/sign-up" >
           <SignUp />
         </Route>
-
         <Route exact path="/timeline" >
           <TimeLine />
+        </Route>
+        <Route exact path="/my-posts" >
+          <MyPosts />
+        </Route>
+        <Route exact path="/user/:id" >
+          <UserPosts />
+        </Route>
+        <Route exact path="/hashtag/:hashtag" >
+          <HashtagPosts />
+        </Route>
+        <Route exact path="/my-likes" >
+          <LikedPosts />
         </Route>
       </Switch>
     </Router>
