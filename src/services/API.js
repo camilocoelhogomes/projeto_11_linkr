ea68e02abeaac9067bca9a1d83e054a882b8d348
PMafra <pmafrarm@gmail.com>
2021-09-17 11:07:24 -0300

    Getting trending hashtags from server: function added in API.js

--- src/services/API.js
+++ src/services/API.js
@@ -4,8 +4,18 @@ const API_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/linkr";
 const SIGNUP = "/sign-up";
 const SIGNIN = "/sign-in";
 
+const createHeaders = (token) => {
+    return { headers: { Authorization: `Bearer ${token}`}};
+}
+
 const logIn = ( body ) => axios.post(API_URL + SIGNIN, body);
 
 const signUp = ( body ) => axios.post(API_URL + SIGNUP, body);
 
-export { signUp, logIn };
+const getTrendingHashtags = ({ token }) => axios.get(`${API_URL}/hashtags/trending`, createHeaders(token));
+
+export { 
+    signUp, 
+    logIn, 
+    getTrendingHashtags,
+};
