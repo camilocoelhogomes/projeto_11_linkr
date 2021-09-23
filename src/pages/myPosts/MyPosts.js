2c646b0857cea9fa646c91d2046aa58b07aa9a25
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-09-23 16:33:33 -0300

    scroll working but with api prolems

--- src/pages/myPosts/MyPosts.js
+++ src/pages/myPosts/MyPosts.js
@@ -6,24 +6,30 @@ import Header from '../../components/Header';
 import Trending from "../../components/Trending";
 import Alert from "../../components/Alert";
 import InfiniteScroll from "react-infinite-scroller";
+import axios from "axios";
+import loading from '../../Assets/img/loading.gif';
 
 export default function MyPosts() {
     const [posts, setPosts] = useState([]);
     const [err, setErr] = useState(null);
     const userInfo = JSON.parse(localStorage.getItem("user"));
+    const [hasMore, setHasMore] = useState(true);
 
-    const getPosts = () => {
-        getUserPosts({ token: userInfo.token, id: userInfo.user.id })
-            .then(res => setPosts(res.data.posts))
+    const getPosts = (id) => {
+        axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v3/linkr/users/${userInfo.user.id}/posts?olderThan=${id}`, { headers: { Authorization: `Bearer ${userInfo.token}` } })
+            .then(res => {
+                setPosts([...posts, ...res.data.posts]);
+                if (res.data.posts.length === 0) {
+                    setHasMore(false);
+                }
+            })
             .catch(() => setErr(true));
     }
 
     useEffect(() => {
-        getPosts();
-        const intervalId =  setInterval(getPosts, 15000);
-        return () => {
-            clearInterval(intervalId);
-        }
+        getUserPosts({ token: userInfo.token, id: userInfo.user.id })
+        .then(res => setPosts(res.data.posts))
+        .catch(() => setErr(true));
     }, []);
 
     if (err) {
@@ -39,17 +45,23 @@ export default function MyPosts() {
                 </header>
                 <div className='main-content'>
                     <div className='posts'>
-                        <InfiniteScroll
-                            pageStart={0}
-                            loadMore={loadFunc}
-                            hasMore={true || false}
-                            loader={<div className="loader" key={0}>Loading ...</div>}
-                        >
-                            {posts.length === 0 
-                                ? <h2>Nenhum post encontrado</h2> 
-                                : posts.map(post => <Post key={post.id} post={post} userInfo={userInfo} getPosts={getPosts} />)
-                            }
-                        </InfiniteScroll>
+                    {posts.length === 0 
+                        ?   <h2>Nenhum post encontrado</h2> 
+                        :   <InfiniteScroll
+                                pageStart={0}
+                                loadMore={() => getPosts(posts[posts.length - 1].id)}
+                                hasMore={hasMore}
+                                loader={
+                                    <div className="loader" key={0}>
+                                        <img src={loading}/>
+                                        Loading more posts...
+                                    </div>
+                                }
+                            >
+                                {posts.map(post => <Post key={post.repostId ? post.repostId : post.id} post={post} userInfo={userInfo} getPosts={getPosts} />)}
+                                
+                            </InfiniteScroll>
+                    }
                     </div>
                     <Trending className='trending' />
                 </div>
