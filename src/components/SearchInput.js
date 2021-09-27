b84d5aed3a11a31f1e090710653c6dd419bddb47
Leonardo <leonardo.otero390@gmail.com>
2021-09-27 15:17:10 -0400

    Change alerts to styled error inputs

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -30,7 +30,7 @@ export default function SearchInput({ parent }) {
                 setUsersLists(users);
             })
         }
-    }, [user])
+    }, [user]);
 
     return (
         <StyledSearchBox parent={parent} >
