9e5a932d6f8bcb7b74d86fb062c3cdfee0614b9c
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-09-27 10:22:04 -0300

    resolve vercel import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import GlobalStyle from "./components/GlobalStyle";
-import { BrowserRouter as Router } from "react-router-dom";
-import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignUp from "./pages/home/sign-up/index";
+import React from "react";
+import LogIn from "./pages/home/sign-in/index";
+import TimeLine from './pages/timeLine/TimeLine';
+import MyPosts from './pages/myPosts/MyPosts';
+import UserPosts from "./pages/userPosts/UserPosts";
+import HashtagPosts from "./pages/hashtagPosts/HashtagPosts";
+import LikedPosts from "./pages/likedPosts/LikedPosts";
 import LinkContext from "./store/LinkContext";
 import LocationPreview from "./components/LocationPreview";
 import LinkPreview from "./components/LinkPreview";
