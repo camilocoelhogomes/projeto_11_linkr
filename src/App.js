95aec6399e524910172ca1378e629ec0a95e7e32
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-09-27 10:25:47 -0300

    resolving the useState import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import GlobalStyle from "./components/GlobalStyle";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignUp from "./pages/home/sign-up/index";
-import React from "react";
+import React, { useState } from "react";
 import LogIn from "./pages/home/sign-in/index";
 import TimeLine from './pages/timeLine/TimeLine';
 import MyPosts from './pages/myPosts/MyPosts';
